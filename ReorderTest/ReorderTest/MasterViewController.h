63e85ae5d0d7ed866e87ed30cc7337bd3311824a
shiami <shiami>
2014-07-03 15:24:37 +0800

    Modify the gesture handling to support both Pan or LongPress. Modify the gesture to support binding to any UIView. It is handy to hook up an UIView in your cell. Add missing framework in the sample project. Modify the example project to fit the new way to setup the gestures.

--- ReorderTest/ReorderTest/MasterViewController.h
+++ ReorderTest/ReorderTest/MasterViewController.h
@@ -7,6 +7,10 @@
 
 #import <UIKit/UIKit.h>
 
+#import "BVReorderTableView.h"
+
 @interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) IBOutlet BVReorderTableView* reorderTableView;
 
 @end
